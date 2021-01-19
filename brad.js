204d94f76a8c2cb9c232cb8cab47107c0ed5ddac
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-01-19 22:25:10 +0500

    Initial commit

---
+++ brad.js
@@ -0,0 +1,112 @@
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
+
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.size = 0;
+	}
+
+	insertFirst(data) {
+		this.head = new Node(data, this.head);
+		this.size++;
+	}
+
+	insertLast(data) {
+		let node = new Node(data);
+		let current;
+
+		if (!this.head) {
+			this.head = node;
+		} else {
+			current = this.head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = node;
+		}
+		this.size++;
+	}
+
+	insertAt(data, index) {
+		// If index is out of range
+		if (index > 0 && index > this.size) {
+			return;
+		}
+
+		// if index should be head
+		if (index === 0) {
+			this.head = new Node(data, this.head);
+			return;
+		}
+
+		const node = new Node(data);
+		let current, prev;
+
+		current = this.head;
+		let count = 0;
+
+		while (count < index) {
+			prev = current;
+			count++;
+			current = current.next;
+		}
+		node.next = current;
+		prev.next = node;
+	}
+
+	getAt(index) {
+		let current = this.head;
+		let count = 0;
+
+		while (current) {
+			if (count == index) console.log(current.data);
+
+			count++;
+			current = current.next;
+		}
+		return null;
+	}
+
+	removeAt(index) {
+		let current = this.head;
+		let prev;
+		let count = 0;
+
+		if (index === 0) {
+			this.head = current.next;
+		} else {
+			while (count < index) {
+				prev = current;
+				current = current.next;
+				count++;
+			}
+
+			prev.next = current.next;
+		}
+
+		this.size--;
+	}
+
+	clearList() {
+		this.head = null;
+	}
+	printList() {
+		let current = this.head;
+
+		while (current) {
+			console.log(current.data);
+			current = current.next;
+		}
+	}
+}
+
+const ll = new LinkedList();
+ll.insertLast(100);
+ll.insertLast(200);
+ll.insertLast(300);
+ll.printList();
